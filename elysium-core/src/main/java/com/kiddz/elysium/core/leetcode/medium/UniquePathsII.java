dcbac7dfa5e2b48b24c587a698c210c9da0b8389
jiaojiaodamowang <298767780@qq.com>
2020-09-08 17:28:44 +0800

    leetCode:[63]Unique Paths II

---
+++ elysium-core/src/main/java/com/kiddz/elysium/core/leetcode/medium/UniquePathsII.java
@@ -0,0 +1,65 @@
+package com.kiddz.elysium.core.leetcode.medium;
+
+/**
+ * A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+ * <p>
+ * The robot can only move either down or right at any point in time. The robot is trying to reach the
+ * bottom-right corner of the grid (marked 'Finish' in the diagram below).
+ * <p>
+ * Now consider if some obstacles are added to the grids. How many unique paths would there be?
+ * <p>
+ * <p>
+ * <p>
+ * An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+ * <p>
+ * Note: m and n will be at most 100.
+ * <p>
+ * Example 1:
+ * Input:
+ * [
+ * [0,0,0],
+ * [0,1,0],
+ * [0,0,0]
+ * ]
+ * Output: 2
+ * <p>
+ * Explanation:
+ * There is one obstacle in the middle of the 3x3 grid above.
+ * There are two ways to reach the bottom-right corner:
+ * 1. Right -> Right -> Down -> Down
+ * 2. Down -> Down -> Right -> Right
+ */
+public class UniquePathsII {
+
+    public static void main(String[] args) {
+        int[][] obstacleGrid = new int[][]{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}};
+        System.out.println(uniquePathsWithObstacles(obstacleGrid));
+        int[][] obstacleGrid2 = new int[][]{{1, 0}};
+        System.out.println(uniquePathsWithObstacles(obstacleGrid2));
+    }
+
+    private static int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        if (obstacleGrid[0][0] == 1) {
+            return 0;
+        }
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else if (i == 0 && j == 0) {
+                    dp[i][j] = 1;
+                } else if (i == 0) {
+                    dp[i][j] = dp[i][j - 1];
+                } else if (j == 0) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
