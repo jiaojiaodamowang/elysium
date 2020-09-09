447d22557172e46484aef7a23b34831f94479dac
jiaojiaodamowang <298767780@qq.com>
2020-09-09 16:46:32 +0800

    leetCode:[64]Minimum Path Sum

---
+++ elysium-core/src/main/java/com/kiddz/elysium/core/leetcode/medium/MinimumPathSum.java
@@ -0,0 +1,47 @@
+package com.kiddz.elysium.core.leetcode.medium;
+
+/**
+ * Given a m x n grid filled with non-negative numbers, find a path from top left to
+ * bottom right which minimizes the sum of all numbers along its path.
+ *
+ * Note: You can only move either down or right at any point in time.
+ *
+ * Example:
+ *
+ * Input:
+ * [
+ *   [1,3,1],
+ *   [1,5,1],
+ *   [4,2,1]
+ * ]
+ *
+ * Output: 7
+ * Explanation: Because the path 1→3→1→1→1 minimizes the sum.
+ */
+public class MinimumPathSum {
+
+    public static void main(String[] args) {
+        int[][] grid = new int[][]{{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};
+        System.out.println(minPathSum(grid));
+    }
+
+    private static int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = grid[i][j];
+                } else if (i == 0) {
+                    dp[i][j] = dp[i][j - 1] + grid[i][j];
+                } else if (j == 0) {
+                    dp[i][j] = dp[i - 1][j] + grid[i][j];
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + +grid[i][j];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
